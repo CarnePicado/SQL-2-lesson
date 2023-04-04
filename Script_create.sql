da2aa08b8c8dee5dc4ad68c6bca72d218be3e3c7
Ivan Aleinikov <cainepicado@gmail.com>
2023-04-04 14:36:32 +0300

    change select requests

--- Script_create.sql
+++ Script_create.sql
@@ -1,44 +1,46 @@
 create table if not exists Genre (
 	id serial primary key,
-	name varchar(64) unique not null
+	"name" varchar(64) unique not null
 	);
-	
+
 create table if not exists Musician (
 	id serial primary key,
-	name varchar(64) unique not null,
-	genre varchar(64) not null
+	"name" varchar(64) unique not null
 	);
 
 create table if not exists Albums (
 	id serial primary key,
-	name varchar(64) not null,
-	date date not null,
-	musician varchar(64) not null
+	"name" varchar(64) not null,
+	"date released" date check("date released">'1900-01-01') not null
 	);
 create table if not exists Songs (
 	id serial primary key,
-	name varchar(64) not null,
-	time varchar(64) not null,
-	album varchar(64) not null,
-	fk_albums_id integer references Albums(id) not null
-);
+	"name" varchar(64) not null,
+	"time" time check("time">'00:00:30') not null,
+	"fk_albums_id" integer references Albums(id) not null
+	);
 
 create table if not exists Compilation (
 	id serial primary key,
-	name varchar(64) not null,
-	year date not null,
-	fk_songs_id integer references Songs(id) not null
-);
+	"name" varchar(64) not null,
+	"year of issue" date check("year of issue">'2000-01-01') not null
+	);
 
-create table if not exists Musician_Genres (
-	genre_id integer references Genre(id),
-	musician_id integer references Musician(id),
-	constraint musician_genres_pk primary key (genre_id, musician_id)
+create table if not exists Musician_genres (
+	"genre_id" integer references Genre(id),
+	"musician_id" integer references Musician(id),
+	constraint musician_genre_pk primary key (genre_id, musician_id)
 	);
 
 create table if not exists Musician_albums (
-	musician_id integer references Musician(id),
-	albums_id integer references Albums(id),
+	"musician_id" integer references Musician(id),
+	"albums_id" integer references Albums(id),
 	constraint musician_albums_pk primary key (musician_id, albums_id)
 	);
-	
+
+create table if not exists Songs_compilation (
+	"songs_id" integer references Songs(id),
+	"compilation_id" integer references Compilation(id),
+	constraint songs_compilation_pk primary key (songs_id, compilation_id)
+	);
+
--- Script_select_2.sql
+++ Script_select_2.sql
@@ -4,11 +4,11 @@ inner join "genre" "g" on "g"."id"="mg"."genre_id"
 group by "g"."name"
 order by count("musician_id") desc;
 
-select count("songs"."name") 
-from "songs" 
-inner join "albums" on "songs"."id"="albums"."id"
-where "albums"."year" between '2019-01-01' and '2020-01-01'
-group by "albums"."year";
+select count("s"."name") 
+from "songs" "s"
+inner join "albums" "a" on "s"."id"="a"."id"
+where "a"."date released" between '2019-01-01' and '2020-01-01'
+group by "a"."date released";
 
 select "a"."name", avg("s"."time")
 from "songs" "s" 
@@ -20,7 +20,8 @@ select "m"."name"
 from "musician" "m" 
 left outer join "musician_albums" "ma" on "ma"."musician_id"="m"."id"
 left outer join "albums" "a" on "ma"."albums_id"="a"."id"
-order by "m"."name";
+where to_char ("a"."date released", 'YYYY')<>'2020';
+	
 
 select distinct "c"."name" 
 from "compilation" "c"
@@ -29,16 +30,16 @@ left outer join "songs" "s" on "s"."id"="sc"."compilation_id"
 left outer join "albums" "a" on "a"."id"="s"."fk_albums_id"
 left outer join "musician_albums" "ma" on "a"."id"="ma"."albums_id"
 left outer join "musician" "m" on "m"."id"="ma"."musician_id"
-where "m"."name" like 'Eazy E';
+where "m"."name" like 'Chuck Berry';
 
-select "a"."name" 
+select distinct "a"."name"
 from "albums" "a"
 right outer join "musician_albums" "ma" on "a"."id"="ma"."albums_id"
 right outer join "musician" "m" on "m"."id"="ma"."musician_id"
 right outer join "musician_genres" "mg" on "m"."id"="mg"."musician_id"
 right outer join "genre" "g" on "g"."id"="mg"."genre_id"
-group by "a"."name"
-having count("mg"."genre_id") > 1;
+group by "a"."name", "mg"."musician_id"
+having count("mg"."musician_id") > 1;
 
 select "s"."name" 
 from "songs" "s"
@@ -51,8 +52,7 @@ from "musician" "m"
 left outer join "musician_albums" "ma" on "ma"."musician_id"="m"."id"
 left outer join "albums" "a" on "a"."id"="ma"."albums_id"
 left outer join "songs" "s" on "s"."fk_albums_id"="a"."id"
-where "time"=(select min("time") from "songs")
-group by "m"."name";
+where "time"=(select min("time") from "songs");
 
 select "a"."name", count("s"."name") as "count"
 from "albums" "a"
@@ -62,9 +62,15 @@ group by "a"."name";
 
 select "a"."name"
 from "albums" "a"
-left outer join "songs" "s" on "a"."id"="s"."fk_albums_id"
+inner join "songs" "s" on "a"."id"="s"."fk_albums_id"
 group by "a"."name"
-having count("s"."name")=2
-order by count("s"."name");
+having count("s"."name")=(select count("s"."name") as "c"
+from "albums" "a"
+inner join "songs" "s" on "a"."id"="s"."fk_albums_id"
+group by "a"."name"
+order by "c"
+limit 1
+);
+
 
 
